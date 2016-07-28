b6f4b235858d3f2cbdb96d752431958a97c8b823
nikhilsu <nikhilsu@thoughtworks.com>
2016-07-29 00:26:42 +0530

    New Verion, Added popup layout with a textbox and a button to the extention

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-searchUrbanDict = function(word){
-  var query = word.selectionText;
-  chrome.tabs.create({url: "https://www.vocabulary.com/dictionary/" + query});
+searchVocabDotComFromContextMenu = function(word){
+	var query = word.selectionText;
+	openVocabularDotComTab(query);
 };
 
+function openVocabularDotComTab(wordToSearch){
+	chrome.tabs.create({url: "https://www.vocabulary.com/dictionary/" + wordToSearch});
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+	document.getElementById('submit').addEventListener('click',function(){
+		var searchText = document.getElementById('searchBox').value;
+		openVocabularDotComTab(searchText);
+	});
+});
+
 chrome.contextMenus.create({
-  title: "Search on Vocabulary.com",
-  contexts:["selection"],
-  onclick: searchUrbanDict
+	title: "Search on Vocabulary.com",
+	contexts:["selection"],
+	onclick: searchVocabDotComFromContextMenu
 });
